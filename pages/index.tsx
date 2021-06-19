7c7277daf5af1bc454f516a046cd5b4b2fb02b67
PavelMGs <pavelmg99@gmail.com>
2021-06-19 19:35:36 +0300

    feat: init

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+
+const index = () => {
+    useEffect(() => {
+        console.log('object');
+    }, [])
+    return (
+        <div>
+            fdg
+        </div>
+    )
+}
+
+export default index;
