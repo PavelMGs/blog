4f4ad1d76f037389bdfe3c2adb196c4a2d1961cb
PavelMGs <pavelmg99@gmail.com>
2021-09-02 11:44:32 +0300

    feat: switch to fallback: false

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -383,7 +383,7 @@ export async function getStaticPaths() {
         params: { id: `${post.id}` },
     }))
 
-    return { paths, fallback: true }
+    return { paths, fallback: false }
 }
 
 export async function getStaticProps({ params }: any) {
